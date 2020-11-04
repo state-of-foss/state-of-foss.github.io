f6c2c0acbf5cf21fe07b3464e84f38246daa5a39
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-11-04 20:23:15 +0530

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  /* Your site config here */
+  plugins: [],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>Hello from Gatsby!</div>;
+};
+
+export default Home;
