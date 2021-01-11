4eb5853c8b44fe46869871d9e48ed35a7bbfcf9b
ubuntu <shreyaagrawal0809>
2021-01-11 18:09:05 +0530

    few fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
       <img src={require('../../static/images/front-img-1.png')} alt="nav-logo"  className="front-page-img-1"/>
 
       <button className={'front-banner-download'}>
-        <p className={'download-text'}>DOWNLOAD</p>
+      <Link to="/#section-9" className={'download-text'}>DOWNLOAD</Link>
       </button>
 
       <div className={'front-banner-nav'}>
