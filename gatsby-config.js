472f837703313a6bc9c7e91e69bf7330c79d6bf7
Shashank Sharma <shashank.sharma98@gmail.com>
2021-01-12 13:31:16 +0530

    Add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,38 @@
 const path = require('path');
 
 module.exports = {
+  siteMetadata: {
+    title: "The State of FOSS in India",
+    titleTemplate: "%s · CivicDataLab",
+    description:
+      "A chronicle of the evolution of the FOSS ecosystem in India, identifying key stakeholders, list out their challenges and recommendations. Supported by Omidyar Network India",
+    url: "https://state-of-foss.in/", // No trailing slash allowed!
+    image: "/static/images/landing-page-img.png", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@StateOfFOSS",
+  },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-2V1FDMQKDM",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
